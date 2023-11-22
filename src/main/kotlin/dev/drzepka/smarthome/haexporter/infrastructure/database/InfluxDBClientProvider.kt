c9544c1a5fb4e1878fad13dca73454d79a9acb89
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2023-11-22 20:06:59 +0100

    Set timeouts when connecting to InfluxDB

--- src/main/kotlin/dev/drzepka/smarthome/haexporter/infrastructure/database/InfluxDBClientProvider.kt
+++ src/main/kotlin/dev/drzepka/smarthome/haexporter/infrastructure/database/InfluxDBClientProvider.kt
@@ -4,7 +4,9 @@ import com.influxdb.client.InfluxDBClientOptions
 import com.influxdb.client.kotlin.InfluxDBClientKotlin
 import com.influxdb.client.kotlin.InfluxDBClientKotlinFactory
 import dev.drzepka.smarthome.haexporter.infrastructure.properties.InfluxDBDataSourceProperties
+import okhttp3.OkHttpClient
 import org.apache.logging.log4j.kotlin.Logging
+import java.time.Duration
 
 class InfluxDBClientProvider(private val properties: InfluxDBDataSourceProperties) {
     val client: InfluxDBClientKotlin
@@ -13,11 +15,17 @@ class InfluxDBClientProvider(private val properties: InfluxDBDataSourcePropertie
 
     init {
         logger.info { "Creating InfluxDB client with ${properties.copy(token = "***")}" }
+        val httpClient = OkHttpClient()
+            .newBuilder()
+            .connectTimeout(Duration.ofSeconds(3))
+            .readTimeout(Duration.ofSeconds(5))
+            .writeTimeout(Duration.ofSeconds(5))
         val options = InfluxDBClientOptions.builder()
             .url(properties.url)
             .bucket(properties.bucket)
             .org(properties.org)
             .authenticateToken(properties.token.toCharArray())
+            .okHttpClient(httpClient)
             .build()
 
         client = InfluxDBClientKotlinFactory.create(options)
